2898fc0f60833bfa8c9fd64b63a5000096e4c1ee
Kitsada <kitsada_t@cmu.ac.th>
2021-09-09 23:45:40 +0700

    6.1

---
+++ src/main/java/se331/lab/rest/dao/OrganizerDao.java
@@ -0,0 +1,11 @@
+package se331.lab.rest.dao;
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import se331.lab.rest.entity.Organizer;
+
+
+public interface OrganizerDao {
+    Page<Organizer> getOrganizer(Pageable pageRequest);
+}
--- src/main/java/se331/lab/rest/repository/OrganizerRepository.java
+++ src/main/java/se331/lab/rest/repository/OrganizerRepository.java
@@ -1,7 +1,11 @@
 package se331.lab.rest.repository;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import se331.lab.rest.entity.Organizer;
 
-public interface OrganizerRepository extends JpaRepository<Organizer, Long> {
+import java.awt.print.Pageable;
+
+public interface OrganizerRepository extends JpaRepository<Organizer,Long> {
+
 }
