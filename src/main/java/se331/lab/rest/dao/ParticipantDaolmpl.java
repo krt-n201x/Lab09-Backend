89fc8bc8fa7c4cc8251bb043ee8860c5b9d6f92b
Kitsada <kitsada_t@cmu.ac.th>
2021-09-10 01:28:04 +0700

    7.3

---
+++ src/main/java/se331/lab/rest/dao/ParticipantDaolmpl.java
@@ -0,0 +1,27 @@
+package se331.lab.rest.dao;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+import se331.lab.rest.entity.Participant;
+import se331.lab.rest.repository.ParticipantRepository;
+
+import java.util.Optional;
+
+
+@Repository
+public class ParticipantDaolmpl implements ParticipantDao{
+    @Autowired
+    ParticipantRepository participantRepository;
+    @Override
+    public Page<Participant> getParticipant(Pageable pageRequest) {
+        return participantRepository.findAll(pageRequest);
+    }
+    @Override
+    public Optional<Participant> findById(Long id) {
+        return participantRepository.findById(id);
+    }
+}
